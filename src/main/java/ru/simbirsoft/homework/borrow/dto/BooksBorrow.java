ddb8943da6202a2f02f43afaa8f19b07d98b7552
Grafon73 <graf.on@mail.ru>
2020-11-13 11:29:07 +0400

    Homework week #1 с исправленными замечаниями

--- src/main/java/ru/simbirsoft/homework/borrow/dto/BooksBorrow.java
+++ src/main/java/ru/simbirsoft/homework/borrow/dto/BooksBorrow.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+import ru.simbirsoft.homework.book.dto.Book;
 import ru.simbirsoft.homework.person.dto.Person;
 
 import javax.validation.Valid;
@@ -19,6 +20,9 @@ public class BooksBorrow {
 
     @Valid
     private Person person;
+
+    @Valid
+    private Book book;
 
     @NotNull(message = "Date cannot be null")
     @ApiModelProperty(value = "Дата взятия книги", example = "2020-10-20 12:49:23.123+03:00")
