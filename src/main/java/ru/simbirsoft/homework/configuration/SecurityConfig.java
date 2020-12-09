869c0b3bded3cf4e79cbf8a056a07d21bc0d232e
Grafon73 <graf.on@mail.ru>
2020-12-09 22:28:31 +0400

    Добавлена защита от Timing Attack: UserDetailsService работает ровно 100 милисекунд, независимо от того, найден пользователь или нет, плюс расшифровка рандомной строки энкриптером аргона, при ненайденном пользователе. Работаю над последним заданием, связанным с BeanPostProcessor.

--- src/main/java/ru/simbirsoft/homework/configuration/SecurityConfig.java
+++ src/main/java/ru/simbirsoft/homework/configuration/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth){
         auth.authenticationProvider(authenticationProvider());
     }
 
---
+++ src/main/java/ru/simbirsoft/homework/configuration/authentication/AuthenticationFailureListener.java
@@ -0,0 +1,21 @@
+package ru.simbirsoft.homework.configuration.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+import ru.simbirsoft.homework.userinterface.service.LoginAttemptService;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    private final LoginAttemptService loginAttemptService;
+
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
+        WebAuthenticationDetails auth = (WebAuthenticationDetails)
+                e.getAuthentication().getDetails();
+        loginAttemptService.loginFailed(auth.getRemoteAddress());
+    }
+}
